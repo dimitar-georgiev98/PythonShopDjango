1c7967a2f43b57ce9813f09094458ec25055d3e9
dimitar_georgiev <dimitar.georgiev99@abv.bg>
2023-02-09 14:49:44 +0200

    added navbar with button for easy navigation

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('new', views.new)
+    path('new', views.new),
+    path('home', views.home),
 ]
--- products/views.py
+++ products/views.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from . models import Product
+from .models import Product
 
 
 def index(request):
     products = Product.objects.all()
     return render(request, 'index.html', {'products': products})
+
+
+def home(request):
+    return render(request, 'home.html')
 
 
 def new(request):
