a14991c3603e317260727dfaaf9a383e43146e8e
dimitar_georgiev <dimitar.georgiev99@abv.bg>
2023-02-09 19:02:55 +0200

    added button for admin page in navbar and changed index/home page

--- pyshop/urls.py
+++ pyshop/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from products.views import home
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
 ]
